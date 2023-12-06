2e1b951dd23f4007fc8d1dbe6a8d5b27030925c9
GabrielDiaz303 <gadiaz@udesa.edu.ar>
2023-12-06 10:25:40 -0300

    initial commit

---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, "core/index.html" )
+def about(request):
+    return render(request, "core/nosotros.html" )
+def perygat(request):
+    return render(request, "core/perrosygatos.html" )
+def contact(request):
+    return render(request, "core/contacto.html" )
+def tienda(request):
+    return render(request, "core/tienda.html" )
